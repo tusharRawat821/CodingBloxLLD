8b09baa46109363194e333beff946fd4401831b9
Tushar Rawat <tusharrawat821@gmail.com>
2021-11-26 15:20:23 +0530

    First commit - Added CodingBloxApp.java

---
+++ src/main/java/com/lld/codingblox/App.java
@@ -0,0 +1,13 @@
+package com.lld.codingblox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
